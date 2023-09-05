828b1cfb5fd79f6d85925ef162b67e86af3b35a0
Jeremy <70193454+seanopegs@users.noreply.github.com>
2023-09-05 18:26:26 +0700

    Update quickSort.py

--- quickSort.py
+++ quickSort.py
@@ -1,17 +1,27 @@
 import random
+import time
+
 def quick_sort(arr):
     if len(arr) <= 1:
         return arr
-
     pivot = arr[len(arr) // 2]
     left = [x for x in arr if x < pivot]
     middle = [x for x in arr if x == pivot]
     right = [x for x in arr if x > pivot]
+    global step_counter
+    step_counter += 1
     return quick_sort(left) + middle + quick_sort(right)
 
-arr = [random.randint(1, 1000) for _ in range(1000)]
+step_counter = 0
+arr = [random.randint(1, 100) for _ in range(100)]
+start_time = time.time()
 sorted_arr = quick_sort(arr)
-print("Unsorted Array: ")
+end_time = time.time()
+
+print("Unsorted Array:")
 print(arr)
-print("Sorted Array: ")
+print("Sorted Array:")
 print(sorted_arr)
+
+print(f"Jumlah langkah: {step_counter}")
+print(f"Waktu yang diperlukan: {end_time - start_time} detik")
